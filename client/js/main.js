b40e395f56957a7afd054c6fcdb63ffbeeb2ece4
rubaqeshta04 <qeshtaruba@gmail.com>
2025-11-08 13:49:09 +0200

    create sidebar #3

---
+++ client/js/main.js
@@ -0,0 +1,37 @@
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  menu.classList.toggle("hidden");
+});
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+    menu.classList.add("hidden");
+  }
+});
+menu.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
+
+const menuBtnSidebar = document.getElementById("menu-btn-sidebar");
+const Sidebar = document.getElementById("sidebar");
+menuBtnSidebar.addEventListener("click", () => {
+  Sidebar.classList.remove("hidden");
+  Sidebar.classList.add("flex");
+  menuBtnSidebar.classList.add("hidden");
+});
+
+const closeSidebar = document.getElementById("close-sidebar");
+closeSidebar.addEventListener("click", () => {
+  Sidebar.classList.remove("flex");
+  Sidebar.classList.add("hidden");
+  menuBtnSidebar.classList.remove("hidden");
+  menuBtnSidebar.classList.add("flex");
+});
